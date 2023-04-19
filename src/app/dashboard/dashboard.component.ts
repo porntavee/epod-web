67178922795e1d0095780ff990468c3df217d140
Porntavee Chantanit <pokub@hotmail.com>
2023-04-19 11:08:33 +0700

    อัพเดท dashboard Deliveries by Country, Delivery Status, Deliveries by Country

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { ServiceProviderService } from '../shared/service-provider.service';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { ToastrService } from 'ngx-toastr';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-dashboard',
@@ -19,46 +20,133 @@ export class DashboardComponent implements OnInit {
 
   }
 
+  criteriaModel: any = {};
+  deliveryStatusModel: any = {WithInTime: 0, OutOfTime: 0, Reject: 0};
+
   ngOnInit(): void {
 
-    //Object.assign(this, this.single);
-
-    // this.serviceProviderService.post('test/getLimit', {}).subscribe(data => {
-    // this.serviceProviderService.post('test/getNumberOfMessagesForMonth', {}).subscribe(data => {
-    //   let model: any = {};
-    //   model = data;
-
-    //   // let dm: any = { name: "ข้อความส่งฟรี", value: model.value };
-
-    //   this.dashboardModel.push({
-    //     "name": "ข้อความส่งฟรี",
-    //     "value": model.quota
-    //   });
-
-    //   this.dashboardModel.push({
-    //     "name": "ส่งข้อความไปแล้ว",
-    //     "value": model.totalUsage
-    //   });
-
-    //   // this.dashboardModel.push({
-    //   //   "name": "ค่าบริการเพิ่มเติม ฿",
-    //   //   "value": 0.8
-    //   // });
-
-    //   this.dashboardModel = [...this.dashboardModel];
-
-    // }, err => {
-    //   // this.spinner.hide();
-    //   // this.toastr.error(err.message, 'แจ้งเตือนระบบ', { timeOut: 1000 });
-    // });
+    const startDate = new Date();
+    const endDate = new Date();
+    // this.criteriaModel.startDate = moment(startDate.setDate(startDate.getDate() - 7)).format('YYYYMMDD');
+    this.criteriaModel.startDate = moment(startDate).format('YYYYMMDD');
+    this.criteriaModel.endDate = moment(endDate).format('YYYYMMDD');
   }
+
+  read() {
+    this.spinner.show();
+
+    let criteria = {
+      "userinformation": this.serviceProviderService.userinformation,
+      "InvoiceDateStart": this.verifyDateTime(this.criteriaModel.startDate),
+      "InvoiceDateEnd": this.verifyDateTime(this.criteriaModel.endDate),
+    }
+
+    let json = JSON.stringify(criteria);
+
+    this.serviceProviderService.post('api/Report/Dashborad_DeliveriesByCountryPie', criteria).subscribe(data => {
+      this.spinner.hide();
+      this.deliveryByCountry = [];
+      let model: any = {};
+      model = data;
+
+      let json1 = JSON.stringify(model.Data);
+
+      model.Data.forEach(element => {
+        this.deliveryByCountry.push(
+          {
+            "name": element.Code + ' ' + element.Percent.toFixed(2) + "%",
+            "value": element.Percent.toFixed(2)
+          });
+      });
+
+      this.deliveryByCountry = [...this.deliveryByCountry];
+
+    }, err => {
+      this.spinner.hide();
+      this.toastr.error(err.message, 'แจ้งเตือนระบบ', { timeOut: 5000 });
+    });
+  
+    this.serviceProviderService.post('api/Report/Dashborad_DeliveryStatusPie', criteria).subscribe(data => {
+      this.spinner.hide();
+      this.deliveryStatus = [];
+      let model: any = {};
+
+      model = data;
+      this.deliveryStatusModel = model.Data[0];
+
+      let json1 = JSON.stringify(model.Data);
+
+      this.deliveryStatus.push(
+        {
+          "name": "Within time limit",
+          "value": model.Data[0].WithInTime || 0
+        });
+
+      // model.Data.forEach(element => {
+      //   this.deliveryStatus.push(
+      //     {
+      //       "name": element.Code + ' ' + element.Percent + "%",
+      //       "value": element.Percent
+      //     });
+      // });
+
+      this.deliveryStatus = [...this.deliveryStatus];
+
+    }, err => {
+      this.spinner.hide();
+      this.toastr.error(err.message, 'แจ้งเตือนระบบ', { timeOut: 5000 });
+    });
+
+    this.serviceProviderService.post('api/Report/Dashborad_DeliveriesByCountryChart', criteria).subscribe(data => {
+      this.spinner.hide();
+      this.deliveryByCountry3 = [];
+      let model: any = {};
+      debugger
+      model = data;
+      // this.deliveryStatusModel = model.Data[0];
+
+      let json1 = JSON.stringify(model.Data);
+
+      // this.deliveryByCountry3.push(
+      //   {
+      //     "name": "Within time limit",
+      //     "value": 100
+      //   });
+
+      model.Data.forEach(element => {
+        this.deliveryByCountry3.push(
+          {
+            "name": element.Code,
+            "value": element.TotalInvoice
+          });
+      });
+
+      this.deliveryByCountry3 = [...this.deliveryByCountry3];
+
+    }, err => {
+      this.spinner.hide();
+      this.toastr.error(err.message, 'แจ้งเตือนระบบ', { timeOut: 5000 });
+    });
+  }
+
+  verifyDateTime(date: string, from: string = ''): any {
+    let dateObj: any = date == "Invalid date" || date == undefined ? undefined : moment(date).format('YYYY-MM-DD');
+
+    return dateObj;
+  }
+
+  numberWithCommas(x) {
+    var parts = x.toString().split(".");
+    parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    return parts.join(".");
+}
 
   view: any[] = [350, 200];
   legend: boolean = true;
   legendPosition: string = 'below';
 
   colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5']
+    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5', '#c8f557', '#8eed4f', '#0ceef5']
   };
 
   // options delivery by country
@@ -119,29 +207,31 @@ export class DashboardComponent implements OnInit {
   ];
 
   deliveryByCountry = [
-    {
-      "name": "20%",
-      "value": 20
-    },
-    {
-      "name": "24%",
-      "value": 24
-    },
-    {
-      "name": "35%",
-      "value": 35
-    },
-    {
-      "name": "21%",
-      "value": 21
-    }
+    // {
+    //   "name": "20%",
+    //   "value": 20
+    // },
+    // {
+    //   "name": "24%",
+    //   "value": 24
+    // },
+    // {
+    //   "name": "35%",
+    //   "value": 35
+    // },
+    // {
+    //   "name": "21%",
+    //   "value": 21
+    // }
   ];
 
   deliveryStatus = [
-    {
-      "name": "Within time limit",
-      "value": 549
-    }
+    // {
+    //   "name": "Within time limit",
+    //   "value": 549
+    // }
+
+    // ไม่เกัี่ยว
     // ,
     // {
     //   "name": "Out of time limit",
@@ -183,14 +273,14 @@ export class DashboardComponent implements OnInit {
         {
           "name": "2011",
           "value": 89400000
-        }, 
+        },
         {
           "name": "1990",
           "value": 62000000
         }
       ]
     },
-  
+
     {
       "name": "February 2022",
       "series": [
@@ -208,7 +298,7 @@ export class DashboardComponent implements OnInit {
         }
       ]
     },
-  
+
     {
       "name": "March 2022",
       "series": [
@@ -301,14 +391,14 @@ export class DashboardComponent implements OnInit {
         {
           "name": "2011",
           "value": 89400000
-        }, 
+        },
         {
           "name": "1990",
           "value": 62000000
         }
       ]
     },
-  
+
     {
       "name": "February 2023",
       "series": [
@@ -326,7 +416,7 @@ export class DashboardComponent implements OnInit {
         }
       ]
     },
-  
+
     {
       "name": "March 2023",
       "series": [
@@ -412,17 +502,17 @@ export class DashboardComponent implements OnInit {
   ];
 
   deliveryByCountry3 = [
-    {
-      "name": "Germany",
-      "value": 8940000
-    },
-    {
-      "name": "USA",
-      "value": 5000000
-    },
-    {
-      "name": "France",
-      "value": 7200000
-    }
+    // {
+    //   "name": "Germany",
+    //   "value": 8940000
+    // },
+    // {
+    //   "name": "USA",
+    //   "value": 5000000
+    // },
+    // {
+    //   "name": "France",
+    //   "value": 7200000
+    // }
   ];
 }
